a67ae9004ba56d0a99d9e872a4720571571b2558
andi9g <andirizky.bayuputra@gmail.com>
2026-04-03 20:33:49 +0700

    oke

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Undangan Digital - {{ $penerima->namapenerima ?? 'NULL' }}</title>
+    <title>Undangan Digital - {{ $penerima->namapenerima ?? 'Tamu Undangan' }}</title>
     <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
     <link href="https://unpkg.com/aos@2.3.4/dist/aos.css" rel="stylesheet">
-    <link rel="icon" href="{{ asset("backgroundku/thumbnail.jpg") }}?v={{ time() }}" type="image/x-icon">
-    <meta property="og:title" content="Undangan Digital - {{ $penerima->namapenerima ?? 'NULL' }}" />
+    <link rel="icon" href="{{ asset("backgroundku/thumbnail.jpg") }}?v={{ time() }}" sizes="any">
+    <link rel="icon" href="{{ asset("backgroundku/thumbnail.jpg") }}?v={{ time() }}" type="image/svg+xml">
+    <meta property="og:title" content="Undangan Digital - {{ $penerima->namapenerima ?? 'Tamu Undangan' }}" />
     <meta property="og:description" content="Undangan digital pernikahan {{ $undangan->identitaspengantin->namapengantinwanita }} & {{ $undangan->identitaspengantin->namapengantinpria }}" />
     <meta property="og:url" content="{{ url()->current() }}" />
     <meta property="og:image" content="{{ asset("backgroundku/thumbnail.jpg") }}?v={{ time() }}" />
