13af4328163f5362e5a4294a5038efcf4bb1093e
andi9g <andirizky.bayuputra@gmail.com>
2026-04-03 21:01:37 +0700

    oke

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,13 +8,6 @@
     <link href="https://unpkg.com/aos@2.3.4/dist/aos.css" rel="stylesheet">
     <link rel="icon" href="{{ asset("backgroundku/thumbnail.jpg") }}?v={{ time() }}" sizes="any">
     <link rel="icon" href="{{ asset("backgroundku/thumbnail.jpg") }}?v={{ time() }}" type="image/svg+xml">
-    <meta property="og:title" content="Undangan Digital - {{ $penerima->namapenerima ?? 'Tamu Undangan' }}" />
-    <meta property="og:description" content="Undangan digital pernikahan {{ $undangan->identitaspengantin->namapengantinwanita }} & {{ $undangan->identitaspengantin->namapengantinpria }}" />
-    <meta property="og:url" content="{{ url()->current() }}" />
-    <meta property="og:image" content="{{ asset("backgroundku/thumbnail.jpg") }}?v={{ time() }}" />
-    <meta property="og:type" content="website" />
-    <meta property="og:image:width" content="1200" />
-    <meta property="og:image:height" content="630" />
     <style>
         html {
             font-size: 15px;
--- resources/views/share.blade.php
+++ resources/views/share.blade.php
@@ -3,15 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Undangan Digital - {{ $penerima->namapenerima ?? 'Tamu Undangan' }}</title>
+    <title>Undangan Digital | {{ $undangan->identitaspengantin->namapengantinwanita }} & {{ $undangan->identitaspengantin->namapengantinpria }}</title>
     <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
     <link href="https://unpkg.com/aos@2.3.4/dist/aos.css" rel="stylesheet">
-    <link rel="icon" href="{{ asset("backgroundku/thumbnail.jpg") }}?v={{ time() }}" sizes="any">
-    <link rel="icon" href="{{ asset("backgroundku/thumbnail.jpg") }}?v={{ time() }}" type="image/svg+xml">
-    <meta property="og:title" content="Undangan Digital - {{ $penerima->namapenerima ?? 'Tamu Undangan' }}" />
-    <meta property="og:description" content="Undangan digital pernikahan {{ $undangan->identitaspengantin->namapengantinwanita }} & {{ $undangan->identitaspengantin->namapengantinpria }}" />
+    <link rel="icon" href="{{ asset("backgroundku/thumbnail.jpg") }} }}" sizes="any">
+    <link rel="icon" href="{{ asset("backgroundku/thumbnail.jpg") }} }}" type="image/svg+xml">
+    <meta property="og:title" content="Undangan Digital | {{ $undangan->identitaspengantin->namapengantinwanita }} & {{ $undangan->identitaspengantin->namapengantinpria }}" />
+    <meta property="og:description" content="Dengan penuh kebahagiaan, kami mengundang {{ $penerima->namapenerima ?? 'Bapak/Ibu/Saudara/i' }} untuk hadir dan memberikan doa restu pada momen pernikahan kami." />
     <meta property="og:url" content="{{ url()->current() }}" />
-    <meta property="og:image" content="{{ asset("backgroundku/thumbnail.jpg") }}?v={{ time() }}" />
+    <meta property="og:image" content="{{ asset("backgroundku/thumbnail.jpg") }} }}" />
     <meta property="og:type" content="website" />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
