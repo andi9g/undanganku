6e0158e8ef06bed395e6572dffdac73c5465e3c1
andi9g <andirizky.bayuputra@gmail.com>
2026-04-03 22:37:12 +0700

    oke

--- resources/views/share.blade.php
+++ resources/views/share.blade.php
@@ -6,12 +6,12 @@
     <title>Undangan Digital | {{ $undangan->identitaspengantin->namapengantinwanita }} & {{ $undangan->identitaspengantin->namapengantinpria }}</title>
     <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
     <link href="https://unpkg.com/aos@2.3.4/dist/aos.css" rel="stylesheet">
-    <link rel="icon" href="{{ asset("backgroundku/thumbnail.jpg") }} }}" sizes="any">
-    <link rel="icon" href="{{ asset("backgroundku/thumbnail.jpg") }} }}" type="image/svg+xml">
+    <link rel="icon" href="{{ asset("backgroundku/thumbnail.jpg") }}?v={{ time() }}" sizes="any">
+    <link rel="icon" href="{{ asset("backgroundku/thumbnail.jpg") }}?v={{ time() }}" type="image/svg+xml">
     <meta property="og:title" content="Undangan Digital | {{ $undangan->identitaspengantin->namapengantinwanita }} & {{ $undangan->identitaspengantin->namapengantinpria }}" />
     <meta property="og:description" content="Dengan penuh kebahagiaan, kami mengundang {{ $penerima->namapenerima ?? 'Bapak/Ibu/Saudara/i' }} untuk hadir dan memberikan doa restu pada momen pernikahan kami." />
     <meta property="og:url" content="{{ url()->current() }}" />
-    <meta property="og:image" content="{{ asset("backgroundku/thumbnail.jpg") }} }}" />
+    <meta property="og:image" content="{{ asset("backgroundku/thumbnail.jpg") }}?v={{ time() }}" />
     <meta property="og:type" content="website" />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
